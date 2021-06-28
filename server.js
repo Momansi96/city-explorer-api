e657e197aff4fe115437562e874a3f48d99244f5
Momansi96 <mansimohammed8@gmail.com>
2021-06-29 00:06:41 +0300

    working on lab7

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express') // require the express package
+const app = express() // initialize your express app instance
+const cors = require('cors');
+require('dotenv').config();
+const weatherData = require('./data/weather.json'); 
+
+app.use(cors()) // after you initialize your express app instance
+ 
+// a server endpoint 
+app.get('/', // our endpoint name
+ function (req, res) { // callback function of what we should do with our request
+  res.send('Hello World') // our endpoint function response
+})
+app.get('/weather', (req, res)=>{
+  res.json(weatherData)
+})
+ 
+app.listen(8000) // kick start the express server to work
